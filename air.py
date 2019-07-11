e253f927b5764e804412eb11d316d6e9bdf3a63e
Izzy Brand <isaiah_brand@brown.edu>
2019-07-11 08:48:58 -0700

    continuing to work in PID control in air.py

--- air.py
+++ air.py
@@ -18,42 +18,48 @@ if __name__ == '__main__':
     imu = IMU()
     servos = Servos()
 
-    pid_roll  = PID(400., 0., 0., control_range=[-150, 150])
-    pid_pitch = PID(-400., 0., 0., control_range=[-150,150])
-    pid_yaw   = PID(0., 0., 0.)
+    pid_roll  = PID(600., 0., 0., control_range=[-250, 250])
+    pid_pitch = PID(-600., 0., 0., control_range=[-250,250])
+    pid_yaw   = PID(10., 0., 0)
     pids = [pid_roll, pid_pitch, pid_yaw]
 
     rpy = np.zeros(3)
     sp_rpy = np.zeros(3)
     cmd = np.array([1000, 1000, 1500, 1500])
 
+    throttle = 1100
+
     tt = t = time.time()
     running = True
     while running:
-        data = imu.read()
-        if data is not None:
-            # get the elapsed time
-            t = time.time()
-            dt = t - tt
-            if dt < 1/10: continue
-            tt = t
+        t = time.time()
+        dt = t - tt
+        tt = t
 
-            # calculate the error from the set_point
-            rpy[[1, 0, 2]] = data
-            err_rpy = sp_rpy - rpy
+        # calculate the error from the set_point
+        rpy[[1, 0, 2]] = imu.read()
+        err_rpy = sp_rpy - rpy
 
-            # run each of the pid controllers
-            cmd_rpy = [pid.step(e, dt) for pid, e in zip(pids, err_rpy)]
+        # run each of the pid controllers
+        cmd_rpy = [pid.step(e, dt) for pid, e in zip(pids, err_rpy)]
 
-            # copy the rates into the servo array
-            cmd[2] = int(cmd_rpy[0]) + 1500
-            cmd[3] = int(cmd_rpy[1]) + 1500
+        # copy the rates into the servo array
+        cmd[0] = throttle #+ cmd_rpy[2]
+        cmd[1] = throttle #- cmd_rpy[2]
+        cmd[2] = 1555 + int(cmd_rpy[0])
+        cmd[3] = 1460 + int(cmd_rpy[1]) 
 
-            # and write the command to the servos
-            print(cmd)
-            # start_time = time.time()
-            # servos.write(cmd)
-            # print(time.time() - start_time)
+        # and write the command to the servos
+        print(dt)
+        servos.write(cmd)
+
+        # wait the remaining time to keep the loop rate constant
+        # elapsed_time = time.time() - start_time
+        # remaining_time = dt - elapsed_time
+        # if remaining_time > 0:
+        #     time.sleep(remaining_time)
+        # else:
+        #     print('Slipping {}'.format(remaining_time))
 
 
     servos.stop_servod()
