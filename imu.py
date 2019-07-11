ca610ea32c5a0230e605c6ce3e042c2f6576397a
Izzy Brand <isaiah_brand@brown.edu>
2019-07-11 08:48:38 -0700

    fixed imu polling by moving imu reading to a parallel process

--- imu.py
+++ imu.py
@@ -1,28 +1,46 @@
 import time
 import RTIMU
+import numpy as np
+from multiprocessing import Process, Array
 
 class IMU:
     def __init__(self):
-        self.settings = RTIMU.Settings('RTIMULib')
-        self.imu = RTIMU.RTIMU(self.settings)
-        print('IMU init' if self.imu.IMUInit() else 'IMU failed to init!')
-        self.imu.setSlerpPower(0.02)
-        self.imu.setGyroEnable(True)
-        self.imu.setAccelEnable(True)
-        self.imu.setCompassEnable(True)
+        self.data = Array('f', np.zeros(3, dtype=float))
+        self.process = Process(target=self.spin, name='IMU', args=(self.data,))
 
+        self.process.start()
+        
     def read(self):
-        return self.imu.getFusionData() if self.imu.IMURead() else None
+        if self.data.acquire():
+            to_return = np.array(self.data[:3])
+            self.data.release()
+        return to_return
+
+    def spin(self, data):
+        settings = RTIMU.Settings('RTIMULib')
+        imu = RTIMU.RTIMU(settings)
+        print('IMU init' if imu.IMUInit() else 'IMU failed to init!')
+        imu.setSlerpPower(0.02)
+        imu.setGyroEnable(True)
+        imu.setAccelEnable(True)
+        imu.setCompassEnable(True)
+        poll_interval = imu.IMUGetPollInterval()/1000.
+
+        while True:
+            if imu.IMURead():
+                raw = imu.getFusionData()
+                if data.acquire(False):
+                    data[:] = raw[:]
+                    data.release()
+
+                time.sleep(poll_interval)
 
 
 if __name__ == '__main__':
     imu = IMU()
 
-
-
     while True:
-        data = imu.read()
-        if data is not None:
-            print(data)
+        print(imu.read())
+        time.sleep(0.01)
 
 
